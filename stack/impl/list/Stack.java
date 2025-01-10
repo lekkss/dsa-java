af762b3562799aaa25e4118b3f82412af13242a5
lekkss <leksguy97@gmail.com>
2025-01-10 19:08:22 +0000

    stack: List impl

--- stack/impl/list/Stack.java
+++ stack/impl/list/Stack.java
@@ -1,5 +1,70 @@
 package stack.impl.list;
 
+import java.util.EmptyStackException;
+
 public class Stack {
+    private ListNode top;
+    private int length;
+
+    public static class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Stack() {
+        top = null;
+        length = 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void push(int data) {
+        ListNode temp = new ListNode(data);
+        temp.next = top;
+        top = temp;
+        length++;
+    }
+
+    public int pop() {
+
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        int result = top.data;
+        top = top.next;
+        length--;
+        return result;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack.length());
+    }
 
 }
