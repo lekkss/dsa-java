1b2df496684c3470c95ce7ad796f5a83ef50ea44
lekkss <leksguy97@gmail.com>
2025-03-03 10:06:48 +0000

    Binary tree: [preorder traversal- (recursive and iterative)]

---
+++ bt/BinaryTree.java
@@ -0,0 +1,72 @@
+package bt;
+
+import java.util.Stack;
+
+public class BinaryTree {
+    private TreeNode root;
+
+    public class TreeNode {
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+
+    }
+
+    public void createBinaryTree() {
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+
+        root = first; // root --> first
+        first.left = second;
+        first.right = third; // second <-- first --> third
+
+        second.left = fourth;
+        second.right = fifth;
+    }
+
+    // recursive approach
+    public void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    // iterative approach
+    public void preOrderIterative(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            System.out.print(temp.data + " ");
+
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.createBinaryTree();
+
+        bt.preOrderIterative(bt.root);
+
+    }
+}
